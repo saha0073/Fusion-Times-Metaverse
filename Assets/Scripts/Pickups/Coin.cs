099b9322c5a5504ee1ec4148d2811ce3cff2ec9e
saha0073 <saha0073@umn.edu>
2022-11-13 22:16:42 +0530

    first commit

---
+++ Assets/Scripts/Pickups/Coin.cs
@@ -0,0 +1,33 @@
+using Fusion;
+using UnityEngine;
+
+public class Coin : NetworkBehaviour, ICollidable {
+
+    [Networked(OnChanged = nameof(OnIsEnabledChangedCallback))]
+    public NetworkBool IsActive { get; set; } = true;
+
+    public Transform visuals;
+
+    public bool Collide(KartEntity kart) {
+        if ( IsActive ) {
+            kart.CoinCount++;
+            Debug.Log("Collide() in Coin.cs");
+
+            IsActive = false;
+            
+            if ( kart.Object.HasStateAuthority ) {
+                Runner.Despawn(Object);
+            }
+        }
+
+        return true;
+    }
+
+    private static void OnIsEnabledChangedCallback(Changed<Coin> changed) {
+        var behaviour = changed.Behaviour;
+        behaviour.visuals.gameObject.SetActive(behaviour.IsActive);
+
+        if ( !behaviour.IsActive )
+            AudioManager.PlayAndFollow("coinSFX", behaviour.transform, AudioManager.MixerTarget.SFX);
+    }
+}
